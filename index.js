ba04cdaaefec44a49adaf7298a40595b58966152
NataBoud <natalia.boudard@gmail.com>
2023-08-31 21:27:26 +0200

    :tada:

---
+++ index.js
@@ -0,0 +1,125 @@
+ 
+var myArray = [
+
+    {'photo': '<img id="img" src="https://randomuser.me/api/portraits/thumb/women/3.jpg"class="rounded" alt=""/>', 
+    'member':'Linda Funke', 
+    'mobile':'06 32 45 34 14',
+    'email':'<a href="#">lindsay.ferguson@reqres.in</a>',
+    'status': '<span class="badge rounded-pill" id="badge">Active</span>',
+    'operation': '<a href="#" class="table-link"><i class="fs-4 bi bi-pencil-square pe-1"></i></a><a href="#" class="table-link"><i  class="fs-5 bi bi-pencil pe-1"></i></a><a href="#" class="table-link"><i class="fs-5 bi bi-trash pe-1"></i></a>',         
+    'action': '<button type="button" class="btn bg-secondary btn-sm">Login</button>'
+    },
+
+    {'photo': '<img id="img" src="https://randomuser.me/api/portraits/thumb/men/77.jpg"class="rounded" alt=""/>', 
+    'member':'Linda Funke', 
+    'mobile':'06 32 45 34 14',
+    'email':'<a href="#">lindsay.ferguson@reqres.in</a>',
+    'status': '<span class="badge rounded-pill" id="badge">Active</span>',
+    'operation': '<a href="#" class="table-link"><i class="fs-4 bi bi-pencil-square pe-1"></i></a><a href="#" class="table-link"><i  class="fs-5 bi bi-pencil pe-1"></i></a><a href="#" class="table-link"><i class="fs-5 bi bi-trash pe-1"></i></a>',         
+    'action': '<button type="button" class="btn bg-secondary btn-sm">Login</button>'
+    },
+
+    {'photo': '<img id="img" src="https://randomuser.me/api/portraits/thumb/women/26.jpg"class="rounded" alt=""/>', 
+    'member':'Linda Funke', 
+    'mobile':'06 32 45 34 14',
+    'email':'<a href="#">lindsay.ferguson@reqres.in</a>',
+    'status': '<span class="badge rounded-pill" id="badge">Active</span>',
+    'operation': '<a href="#" class="table-link"><i class="fs-4 bi bi-pencil-square pe-1"></i></a><a href="#" class="table-link"><i  class="fs-5 bi bi-pencil pe-1"></i></a><a href="#" class="table-link"><i class="fs-5 bi bi-trash pe-1"></i></a>',          
+    'action': '<button type="button" class="btn bg-secondary btn-sm">Login</button>'
+    },
+
+    {'photo': '<img id="img" src="https://randomuser.me/api/portraits/thumb/men/5.jpg"class="rounded" alt=""/>', 
+    'member':'Linda Funke', 
+    'mobile':'06 32 45 34 14',
+    'email':'<a href="#">lindsay.ferguson@reqres.in</a>',
+    'status': '<span class="badge rounded-pill" id="badge">Active</span>',
+    'operation': '<a href="#" class="table-link"><i class="fs-4 bi bi-pencil-square pe-1"></i></a><a href="#" class="table-link"><i  class="fs-5 bi bi-pencil pe-1"></i></a><a href="#" class="table-link"><i class="fs-5 bi bi-trash pe-1"></i></a>',          
+    'action': '<button type="button" class="btn bg-secondary btn-sm">Login</button>'
+    },
+
+    {'photo': '<img id="img" src="https://randomuser.me/api/portraits/thumb/women/8.jpg"class="rounded" alt=""/>', 
+    'member':'Linda Funke', 
+    'mobile':'06 32 45 34 14',
+    'email':'<a href="#">lindsay.ferguson@reqres.in</a>',
+    'status': '<span class="badge rounded-pill" id="inactive">Inactive</span>',
+    'operation': '<a href="#" class="table-link"><i class="fs-4 bi bi-pencil-square pe-1"></i></a><a href="#" class="table-link"><i  class="fs-5 bi bi-pencil pe-1"></i></a><a href="#" class="table-link"><i class="fs-5 bi bi-trash pe-1"></i></a>',       
+    'action': '<button type="button" class="btn bg-secondary btn-sm">Login</button>'
+    },
+
+    {'photo': '<img id="img" src="https://randomuser.me/api/portraits/thumb/men/62.jpg"class="rounded" alt=""/>', 
+    'member':'Linda Funke', 
+    'mobile':'06 32 45 34 14',
+    'email':'<a href="#">lindsay.ferguson@reqres.in</a>',
+    'status': '<span class="badge rounded-pill" id="badge">Active</span>',
+    'operation': '<a href="#" class="table-link"><i class="fs-4 bi bi-pencil-square pe-1"></i></a><a href="#" class="table-link"><i  class="fs-5 bi bi-pencil pe-1"></i></a><a href="#" class="table-link"><i class="fs-5 bi bi-trash pe-1"></i></a>',        
+    'action': '<button type="button" class="btn bg-secondary btn-sm">Login</button>'
+    },
+
+    {'photo': '<img id="img" src="https://randomuser.me/api/portraits/thumb/women/80.jpg"class="rounded" alt=""/>', 
+    'member':'Linda Funke', 
+    'mobile':'06 32 45 34 14',
+    'email':'<a href="#">lindsay.ferguson@reqres.in</a>',
+    'status': '<span class="badge rounded-pill" id="badge">Active</span>',
+    'operation': '<a href="#" class="table-link"><i class="fs-4 bi bi-pencil-square pe-1"></i></a><a href="#" class="table-link"><i  class="fs-5 bi bi-pencil pe-1"></i></a><a href="#" class="table-link"><i class="fs-5 bi bi-trash pe-1"></i></a>',          
+    'action': '<button type="button" class="btn bg-secondary btn-sm">Login</button>'
+    },
+
+    {'photo': '<img id="img" src="https://randomuser.me/api/portraits/thumb/women/55.jpg"class="rounded" alt=""/>', 
+    'member':'Linda Funke', 
+    'mobile':'06 32 45 34 14',
+    'email':'<a href="#">lindsay.ferguson@reqres.in</a>',
+    'status': '<span class="badge rounded-pill" id="inactive">Inactive</span>',
+    'operation': '<a href="#" class="table-link"><i class="fs-4 bi bi-pencil-square pe-1"></i></a><a href="#" class="table-link"><i  class="fs-5 bi bi-pencil pe-1"></i></a><a href="#" class="table-link"><i class="fs-5 bi bi-trash pe-1"></i></a>',        
+    'action': '<button type="button" class="btn bg-secondary btn-sm">Login</button>'
+    },
+
+    {'photo': '<img id="img" src="https://randomuser.me/api/portraits/thumb/women/45.jpg"class="rounded" alt=""/>', 
+    'member':'Linda Funke', 
+    'mobile':'06 32 45 34 14',
+    'email':'<a href="#">lindsay.ferguson@reqres.in</a>',
+    'status': '<span class="badge rounded-pill" id="badge">Active</span>',
+    'operation': '<a href="#" class="table-link"><i class="fs-4 bi bi-pencil-square pe-1"></i></a><a href="#" class="table-link"><i  class="fs-5 bi bi-pencil pe-1"></i></a><a href="#" class="table-link"><i class="fs-5 bi bi-trash pe-1"></i></a>',          
+    'action': '<button type="button" class="btn bg-secondary btn-sm">Login</button>'
+    },
+
+    {'photo': '<img id="img" src="https://randomuser.me/api/portraits/thumb/men/37.jpg"class="rounded" alt=""/>', 
+    'member':'Linda Funke', 
+    'mobile':'06 32 45 34 14',
+    'email':'<a href="#">lindsay.ferguson@reqres.in</a>',
+    'status': '<span class="badge rounded-pill" id="badge">Active</span>',
+    'operation': '<a href="#" class="table-link"><i class="fs-4 bi bi-pencil-square pe-1"></i></a><a href="#" class="table-link"><i  class="fs-5 bi bi-pencil pe-1"></i></a><a href="#" class="table-link"><i class="fs-5 bi bi-trash pe-1"></i></a>',         
+    'action': '<button type="button" class="btn bg-secondary btn-sm">Login</button>'
+    },
+]
+
+buildTable(myArray)
+
+function buildTable(data){
+    var table = document.getElementById('myTable');                                     
+    for (var i = 0; i < data.length; i++){
+        var row = `<tr>
+                        <td>${data[i].photo}</td>
+                        <td>${data[i].member}</td>
+                        <td>${data[i].mobile}</td>
+                        <td>${data[i].email}</td>
+                        <td>${data[i].status}</td>
+                        <td>${data[i].operation}</td>
+                        <td>${data[i].action}</td>
+                       
+                        
+                    </tr>`
+        table.innerHTML += row 
+    }
+}
+
+
+// $.ajax({
+//     method:'GET',
+//     url:'https://reqres.in/api/users',
+//     success:function(response){
+//         myArray = response.data
+//         buildTable(myArray)
+//         console.log(myArray)
+//     }
+// })
+
